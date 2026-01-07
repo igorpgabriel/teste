83d78b63cf67d5bdfd320bbe4741bcbf591da06c
igor.gabriel <igor.gabriel@grupovamos.com.br>
2026-01-07 11:25:54 -0300

    alterado

--- function_app.py
+++ function_app.py
@@ -1,16 +1,10 @@
 import azure.functions as func
-import datetime
 import logging
-# Adicione o ponto antes de main se o arquivo estiver na mesma pasta
-from .main import funcao_teste 
+from .main import funcao_teste
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True)
 def timer_trigger(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('O agendamento está atrasado!')
-    
-    logging.info('Iniciando chamada da funcao_teste...')
+    logging.info('Executando função de teste...')
     funcao_teste()
-    logging.info('Script executado com sucesso.')
