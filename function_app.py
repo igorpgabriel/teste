973353b316c07cc0118207c5e90e1ee868df5203
Igor Gabriel <81106970+igorpgabriel@users.noreply.github.com>
2026-01-07 16:51:27 -0300

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -1,10 +1,20 @@
 import azure.functions as func
 import logging
-from .main import funcao_teste
+import datetime
 
 app = func.FunctionApp()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True)
-def timer_trigger(myTimer: func.TimerRequest) -> None:
-    logging.info('Executando função de teste...')
-    funcao_teste()
+try:
+    from .main import funcao_teste
+except Exception as e:
+    logging.error(f"Erro ao importar main.py: {e}")
+
+@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False)
+def timer_trigger_teste(myTimer: func.TimerRequest) -> None:
+    logging.info('O gatilho do timer foi disparado.')
+    
+    try:
+        funcao_teste()
+        logging.info('funcao_teste executada com sucesso.')
+    except Exception as e:
+        logging.error(f"Erro na execucao da funcao_teste: {e}")
