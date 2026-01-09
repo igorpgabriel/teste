8e4472d09c180eb82a2dc80eedfa01abadf8c007
Igor Gabriel <81106970+igorpgabriel@users.noreply.github.com>
2026-01-09 15:48:41 -0300

    Replace print with logging for execution log

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-# main.py
 import logging
 
 def funcao_teste():
-    # Em vez de print("Olá"), use:
     logging.info("--- Log de Execução: A lógica principal rodou com sucesso! ---")
     
-    # Se quiser registrar um erro:
-    # logging.error("Algo deu errado aqui")
+    
